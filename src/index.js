bd349f9a93a6d7b9292bcbab751649c4e45f1aa0
Macbaren <macbaren123@gmail.com>
2022-02-08 21:24:12 +0300

    feat fin

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    return matrix
+        ? matrix.map((arr, ind) => (ind % 2 !== 0 ? arr.reverse() : arr)).flat()
+        : [];
+};
